c3cda0939ec0e35962ad7163512a2df25d11a0b9
Phenolemox <xafizzov@ya.ru>
2026-06-20 16:35:07 +0700

    Add pokerbot achievements registry

---
+++ pokerbot_stage1/repo/app/game/achievements.py
@@ -0,0 +1,8 @@
+ACHIEVEMENTS = [
+    {"code": "first_blood", "title": "Первый выстрел", "description": "Сыграть первую игру"},
+    {"code": "pair_hunter", "title": "Охотник за парами", "description": "Выбить пару 10 раз"},
+    {"code": "straight_road", "title": "Дорога через салун", "description": "Собрать стрит"},
+    {"code": "flush_rider", "title": "Всадник флеша", "description": "Собрать флеш"},
+    {"code": "royal_legend", "title": "Легенда стола", "description": "Собрать роял-флеш"},
+    {"code": "duelist", "title": "Дуэлянт", "description": "Победить в первой дуэли"},
+]
